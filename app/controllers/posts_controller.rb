2202aa233e4c946618ec24b3548ca761bcebbe33
Rebekah <rbk1008@Gmail.com>
2022-02-04 15:36:16 -0700

    commit tree a14b8803286a97da185f145347c7659090b7a212

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,18 @@
 class PostsController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+ 
 
   def show
     post = Post.find(params[:id])
-    
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
+  rescue ActiveRecord::RecordInvalid => invalid
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
   end
 
   private
@@ -20,4 +21,5 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+    
 end
